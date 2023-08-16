d8c56c2d78b7a316a545501aec9b52f015b6eb30
Suthipong C.vidhayakul <lufael.sc@gmail.com>
2023-08-16 17:22:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import AnimalShow from "./AnimalShow";
+import { useState } from "react";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  // const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log("Button clicked");
+    // setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
